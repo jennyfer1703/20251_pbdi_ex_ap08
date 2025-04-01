75e95ad5b9dd7cebe5390871c346e07e7183c7c2
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-03-31 23:36:04 -0300

    feat: implementa conversao de temperatura de Celsius para Fahrenheit

--- Atividades/ap08.sql
+++ Atividades/ap08.sql
@@ -1,12 +1,26 @@
 DO
 $$ 
 DECLARE 
-    v_real NUMERIC(10,2);
+    temp_celsius NUMERIC(10,2);
+    temp_fahrenheit NUMERIC(10,2);
 BEGIN
-    v_real := RANDOM() * 9 + 1;
-    RAISE NOTICE 'Valor real gerado: %', v_real;
+    temp_celsius := valor_aleatorio_entre(20, 30);
+    temp_fahrenheit := temp_celsius * 9/5 + 32;
+    RAISE NOTICE 'Temperatura: %°C = %°F', ROUND(temp_celsius, 2), ROUND(temp_fahrenheit, 2);
 END 
 $$
+
+
+
+--DO
+--$$ 
+--DECLARE 
+--    v_real NUMERIC(10,2);
+--BEGIN
+--    v_real := RANDOM() * 9 + 1;
+--   RAISE NOTICE 'Valor real gerado: %', v_real;
+--END 
+--$$
 
 
 --DO 
