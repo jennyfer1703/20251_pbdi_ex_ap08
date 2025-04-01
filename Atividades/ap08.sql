067d7c8cedfe1baf925d02324abef3a4c2c4e429
Jennyfer Santos <jennyfersantos173@gmail.com>
2025-04-01 00:20:48 -0300

    feat: implementa calculo do delta de uma equacao de segundo grau

--- Atividades/ap08.sql
+++ Atividades/ap08.sql
@@ -1,16 +1,33 @@
+---- 1.4 - Cálculo do Delta de uma equação do segundo grau
 DO
-$$ 
-DECLARE 
-    temp_celsius NUMERIC(10,2);
-    temp_fahrenheit NUMERIC(10,2);
-BEGIN
-    temp_celsius := valor_aleatorio_entre(20, 30);
-    temp_fahrenheit := temp_celsius * 9/5 + 32;
-    RAISE NOTICE 'Temperatura: %°C = %°F', ROUND(temp_celsius, 2), ROUND(temp_fahrenheit, 2);
-END 
+$$
+DECLARE
+    a INT := valor_aleatorio_entre(0, 20);
+    b INT := valor_aleatorio_entre(0, 20);
+    c INT := valor_aleatorio_entre(0, 20);
+    delta NUMERIC(10,2);BEGIN
+    RAISE NOTICE '%x² + %x + % = 0', a, b, c;
+    IF a = 0 THEN 
+        RAISE NOTICE 'Não é equação do segundo grau';
+    ELSE 
+        delta := b ^ 2 - 4 * a * c;
+        RAISE NOTICE 'Delta: %', delta;
+    END IF; 
+END
 $$
 
 
+--DO
+--$$ 
+--DECLARE 
+--    temp_celsius NUMERIC(10,2);
+--    temp_fahrenheit NUMERIC(10,2);
+--BEGIN
+--    temp_celsius := valor_aleatorio_entre(20, 30);
+--    temp_fahrenheit := temp_celsius * 9/5 + 32;
+--    RAISE NOTICE 'Temperatura: %°C = %°F', ROUND(temp_celsius, 2), ROUND(temp_fahrenheit, 2);
+--END 
+--$$
 
 --DO
 --$$ 
